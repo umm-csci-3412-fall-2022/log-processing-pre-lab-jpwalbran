ce82706eaeef98830e372a2577500308e66781d7
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-09-06 16:00:16 -0500

    Finished the wrap_contents.sh script
    Added specifically named variables for the input parameters, as well as a
    cat command to combine the files together, piped into the correct target file.

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+# Rename the input parameters into helpfully named variables
+fileContents="$1"
+headerFooterFileBaseName="$2"
+targetFileName="$3"
+
+# Concatenate the files based on the parameters
+# This should concatenate the header, then the contents, then the footer
+# outputting into the desired target file
+cat "${headerFooterFileBaseName}_header.html" "$fileContents" "${headerFooterFileBaseName}_footer.html" > "$targetFileName"
