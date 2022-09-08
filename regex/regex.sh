f9c9c3c0ded00185cf55ceb98e0547e45465ed19
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-09-07 19:37:03 -0500

    Add prefix extraction to regex.sh
    Changed permissions to executable, and added functionality to regex.sh
    that allows for the extraction of the base file prefix for the output
    based on the command line argument (eg. r0, r1, etc).

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-`
+# rename the input file from the command line arguments
+inputFile="$1"
+
+# get the relevant numbering of the filename (eg. r0, r1, r2) from the inputFilePath
+# Based on FerranB's stackOverflow answer here:
+# https://stackoverflow.com/questions/428109/extract-substring-in-bash
+baseFile=$(echo "$inputFile" | cut -d'_' -f 1)
+
+echo "$baseFile"
