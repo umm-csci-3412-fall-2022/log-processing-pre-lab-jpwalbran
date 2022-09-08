026405773afabb93c63217769af5625927db6bf5
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-09-07 20:16:13 -0500

    Added the patterns to match the given regex groups
    Added variables representing the patterns to match the components
    of the inputs to match to the groups for the resulting replacement.
    Tested with regexr.com and the sample inputs.

--- regex/regex.sh
+++ regex/regex.sh
@@ -8,4 +8,32 @@ inputFile="$1"
 # https://stackoverflow.com/questions/428109/extract-substring-in-bash
 baseFile=$(echo "$inputFile" | cut -d'_' -f 1)
 
-echo "$baseFile"
+# Construct the regex replacement sections for the various inputs
+
+###################R0 patterns###################
+
+# Gets the name and stores it in a group
+nameRegex="^\* \([A-Za-z]*\)"
+
+# Gets the favorite breakfast food and stores it in a group
+breakfastSnackRegex="^.*, \([A-Za-z]*\)"
+
+##################R1 patterns#####################
+
+# Matches the pattern "I am <name>." and captures the name in a group
+introductionRegex="^I am \([A-Za-z]*\)\."
+
+# Matches the regex "favorite sandwich is <favorite sandwich>"
+# and captures the output into a group.
+favoriteSandwichRegex="favorite sandwhich is \([A-Za-z]*\)\.$"
+
+################R2 patterns#####################
+
+# Gets the sandwich toppings, storing them to a group
+sandwichToppingRegex=" \([A-Za-z]*\.\)"
+
+# Gets the "for here" or "to go" pattern at the end of the string
+eatingLocationRegex="\.\s\([ft].*\)"
+
+
+
