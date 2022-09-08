68e8886653b3a059c6a1b318b44e0c50825fee27
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-09-08 15:19:24 -0500

    Fix the regex for the second file to fix output.
    Added anchors to the regex pattern in the sed command for the second input file (r1).
    This fixed the output when run locally.

--- regex/regex.sh
+++ regex/regex.sh
@@ -4,6 +4,6 @@
 
 sed -n -E "s/\* ([A-Za-z]*), ([A-Za-z]*)/1. \1\n2. \2\n/p" < r0_input.txt > r0_output.txt
 
-sed -n -E "s/\* I am ([A-Za-z]*)\. My favorite sandwich is ([A-Za-z]*)/1. \1\n2. \2\n/p" < r1_input.txt > r1_output.txt
+sed -n -E "s/^\* I am ([A-Za-z]*)\. My favorite sandwich is ([A-Za-z]*)\.$/1. \1\n2. \2\n/p" < r1_input.txt > r1_output.txt
 
 sed -n -E "s/\* .* ([A-Za-z]*\..*)\s([ft].*)/1. \1\n2. \2\n/p" < r2_input.txt > r2_output.txt
